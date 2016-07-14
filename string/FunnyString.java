06b9379e94d06980e85ce3f1d2ca89726bc50a32
PriyatamRoy <roy28.priyatam@gmail.com>
2016-07-14 06:50:17 -0400

    Update FunnyString.java

--- string/FunnyString.java
+++ string/FunnyString.java
@@ -1,26 +1,30 @@
 package misc;
 
-public class FunnyString {
+public class FunnyString {public static void main(String[] args) {
 
-	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
-		String str = "abdgtwyz";
-		
-		int i = 1;
-		int j = str.length()-2;
-		boolean isFunny = true;
-		
-		while(i<str.length()-1){
-			
-			if(str.charAt(i)-str.charAt(i-1) + str.charAt(j)-str.charAt(j+1) != 0){
-				isFunny = false;
-				break;
+		int n = sc.nextInt();
+
+		for (int c = 0; c < n; c++) {
+			String str = sc.next();
+
+			String rev = new StringBuilder(str).reverse().toString();
+
+			int i = 1;
+			boolean isFunny = true;
+			while (i < str.length() - 1) {
+
+				if (Math.abs(str.charAt(i) - str.charAt(i - 1)) != Math.abs(rev.charAt(i) - rev.charAt(i - 1))) {
+					isFunny = false;
+					break;
+				}
+				i++;
 			}
-			i++;
-			j--;
+			if (isFunny)
+				System.out.println("Funny");
+			else
+				System.out.println("Not Funny");
 		}
-		
-		System.out.println(isFunny);
 	}
-
 }
